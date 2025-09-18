bce2957cff31d59f631bd5c9dfc040966911871f
Joseph Bagheri <39276421+Josephshark@users.noreply.github.com>
2025-09-18 21:32:32 +0000

    finished assignment

--- Assignment 1/LearnedRectangle.py
+++ Assignment 1/LearnedRectangle.py
@@ -25,11 +25,37 @@ E.g., a return value from next(generate_example()) may be
 class LearnedRectangle:
     def __init__(self):
         # Your code here to initialize the object.
+        self.dimension = 0
+        self.min_values = []
+        self.max_values = []
 
     def learn(self, m):
         # Your code here to learn the target rectangle. You should
         # get the m examples by invoking next(generate_example()).
         # You can assume that m is an integer >= 1.
+        
+        dimenision = None
+        for i in range(m):
+            # Get the next example.
+            example, ispos = next(generate_example())
+
+            # Your code here to initialize your learned rectangle.
+            
+            # Your code here to process this example.
+            if i == 0:
+                self.dimension = len(example)
+            
+            if ispos:
+                for d in range(self.dimension):
+                    if i == 0:
+                        self.min_values.append(example[d])
+                        self.max_values.append(example[d])
+                    else:
+                        if example[d] < self.min_values[d]:
+                            self.min_values[d] = example[d]
+                        if example[d] > self.max_values[d]:
+                            self.max_values[d] = example[d]
+
 
     def checkgoodness(self, n, k, epsilon):
         # Your code here for the following, whose intent is to check
@@ -37,6 +63,20 @@ class LearnedRectangle:
         # Initialize a counter, and perform the following n times.
         # For k examples, check whether the proportion of those k
         # that are misclassified by your learned rectangle is > epsilon.
+        for number_of_sets in range(n):
+            misclassified_set_count = 0
+            misclassified_example_count = 0
+            for example_index in range(k):
+                example, ispos = next(generate_example())
+                is_learned_pos = True
+                for d in range(self.dimension):
+                    if example[d] < self.min_values[d] or example[d] > self.max_values[d]:
+                        is_learned_pos = False
+                        break
+                if is_learned_pos != ispos:
+                    misclassified_example_count += 1
+            if misclassified_example_count > k * epsilon:
+                misclassified_set_count += 1
         # If yes, increase your counter by 1.
         # Return the value of your counter.
         #
@@ -49,4 +89,8 @@ class LearnedRectangle:
         # As 3 > 5 x 0.2, you will increase your counter by 1. And you
         # will return 1, i.e., the value of the counter, as your result.
 
-        return 0 # replace with correct return value
+        return misclassified_set_count # replace with correct return value
+
+learner = LearnedRectangle()
+learner.learn(1000)
+print(learner.checkgoodness(10, 1000, 0.001))
